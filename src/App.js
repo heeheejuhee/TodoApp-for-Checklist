a345a280e4a60dda098ea11e165da9ae22c67f89
Juhee Han <200859841@cit3-501w-15.auckland.natcoll.ac.nz>
2019-07-31 16:09:54 +1200

    Done

--- src/App.js
+++ src/App.js
@@ -4,40 +4,77 @@ import './App.css';
 class TodoApp extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+        todos : [
+        {
+          id:1,
+          content:'Hi Juhee'
+        },
+        {
+          id:2,
+          content:'How are you '
+        },
+        {
+          id:3,
+          content:'How is the weather'
+        }
+      ],
 
-    this.todos = [
-      {
-        id:1,
-        content:'Hi Juhee'
-      },
-      {
-        id:2,
-        content:'How are you '
-      },
-      {
-        id:3,
-        content:'How is the weather'
-      }
-    ]
+      todoInputValue:''
+    }
+  }
 
+  handleTodoInputChange = (e) => { 
+
+    this.setState({
+      todoInputValue:e.target.value
+    })
+  }
+
+  handleAddTodoClick = (e) => {
+    e.preventDefault();
+
+    var newTodo = {
+      id:Date.now(),
+      content: this.state.todoInputValue
+    };
+
+    var newList = [newTodo,...this.state.todos]
+    this.setState({
+      todos:newList,
+      todoInputValue:''
+    })
+  }
+
+  removeTodo = (id) => {
+    var todos = this.state.todos;
+
+    var filteredList = todos.filter((todo) => {
+      return todo.id != id;
+    });
+
+    this.setState({
+      todos:filteredList
+    })
   }
 
 
   render() {
+
     return(
       <div className="wrap">
           <h1>Daily Todo Lists</h1>
 
           <form>
-            <input type="text"/>
-            <input type="submit"/>
+            <input className="todo-input" type="text" value={this.state.todoInputValue} onChange={this.handleTodoInputChange }/>
+            <input className="add-todo" onClick={this.handleAddTodoClick} type="submit"/>
           </form>
 
           <ul>
             {
-              this.todos.map(function(todo){
+              this.state.todos.map((todo) => {
                 return (
-                  <li>{todo.content}</li>
+                  <li><div onClick={(e) => { this.removeTodo(todo.id) }}>x</div> {todo.content}</li>
                 );
               })
             }  
